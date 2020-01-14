8d06c42649f1068080a549372e3c3c857a4f5c1b
Ricardo Carballo <rcarbaleq2@gmail.com>
2020-01-13 19:51:08 -0800

    Initial spring boot project

---
+++ src/main/java/co/bibleit/springboot/bibleitspringboot/BibleitSpringBootApplication.java
@@ -0,0 +1,13 @@
+package co.bibleit.springboot.bibleitspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleitSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleitSpringBootApplication.class, args);
+	}
+
+}
