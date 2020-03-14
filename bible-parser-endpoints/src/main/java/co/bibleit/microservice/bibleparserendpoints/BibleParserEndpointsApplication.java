553143f72dc60660ffcdd08b7ae0e85094488c6a
Ricardo Carballo <rcarbaleq2@gmail.com>
2020-03-14 12:50:58 -0700

    Initial bible-parser-endpoints commit

---
+++ bible-parser-endpoints/src/main/java/co/bibleit/microservice/bibleparserendpoints/BibleParserEndpointsApplication.java
@@ -0,0 +1,13 @@
+package co.bibleit.microservice.bibleparserendpoints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleParserEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleParserEndpointsApplication.class, args);
+	}
+
+}
---
+++ bible-parser-endpoints/src/test/java/co/bibleit/microservice/bibleparserendpoints/BibleParserEndpointsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.bibleit.microservice.bibleparserendpoints;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleParserEndpointsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
