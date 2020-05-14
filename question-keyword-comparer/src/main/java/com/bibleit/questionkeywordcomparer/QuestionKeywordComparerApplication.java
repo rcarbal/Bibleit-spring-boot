fb685692094233a09d9bd8a6aed1c9bb8dd68d2f
rcarbal <rcarbaleq2@gmail.com>
2020-05-14 07:33:30 -0700

    initial question-keyword-comparer commit

---
+++ question-keyword-comparer/src/main/java/com/bibleit/questionkeywordcomparer/QuestionKeywordComparerApplication.java
@@ -0,0 +1,13 @@
+package com.bibleit.questionkeywordcomparer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionKeywordComparerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionKeywordComparerApplication.class, args);
+	}
+
+}
---
+++ question-keyword-comparer/src/test/java/com/bibleit/questionkeywordcomparer/QuestionKeywordComparerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibleit.questionkeywordcomparer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionKeywordComparerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
