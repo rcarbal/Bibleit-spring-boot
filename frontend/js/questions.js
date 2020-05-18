1111a8519c66032881803c4aeb0fd8eba2e5c8aa
rcarbal <rcarbaleq2@gmail.com>
2020-05-17 22:25:58 -0700

    Add click listener to question.html on list of questions

--- frontend/js/questions.js
+++ frontend/js/questions.js
@@ -1,9 +1,15 @@
 const live_server = "https://bibleit.online";
 const test_server = "http://localhost:8000";
 let EVENT_LISTENER_RUNNING = false;
+let currentData;
 
 let input = document.getElementById('answer');
 let previewDiv = document.getElementById('previewDiv');
+
+// $(document).click(function(event) {
+//     var text = $(event.target).text();
+//     console.log("Clicked")
+// });
 
 function echoWord(){
     
@@ -49,20 +55,34 @@ function getPreviewQuestion(inputValue){
         if (previewDiv.hasChildNodes()){
             previewDiv.innerHTML = "";
         }
-        
+
+        let index = 0;
         if (response.data.length > 0){
             response.data.forEach((data)=>{
+                
+                let questionDataContainer  = document.createElement('div')
 
+                // Set click listener
+                questionDataContainer.onclick = (event)=>{
+                    console.log(event)
+                }
+
+                questionDataContainer.setAttribute('data-index', index);
                 let question = document.createElement('strong')
                 let matches = document.createElement('div')
                 question.innerHTML = data['question']
                 matches.innerHTML = data['matches']
-                previewDiv.appendChild(question)
-                previewDiv.appendChild(matches)
+                questionDataContainer.classList.add("set-border")
+                questionDataContainer.appendChild(question)
+                questionDataContainer.appendChild(matches)
+                previewDiv.appendChild(questionDataContainer)
+                index++
             })
         }
         EVENT_LISTENER_RUNNING = false;  
     })
+}
 
-
+function setQuestionInformation(event){
+    console.log(event)
 }
