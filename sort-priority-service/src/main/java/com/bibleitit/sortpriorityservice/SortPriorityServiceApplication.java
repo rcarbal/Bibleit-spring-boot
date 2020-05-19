f4a6586232e24da152c57143150d014fdb4df031
rcarbal <rcarbaleq2@gmail.com>
2020-05-19 12:54:52 -0700

    inital sort-priority-service commit

---
+++ sort-priority-service/src/main/java/com/bibleitit/sortpriorityservice/SortPriorityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bibleitit.sortpriorityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortPriorityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortPriorityServiceApplication.class, args);
+	}
+
+}
---
+++ sort-priority-service/src/test/java/com/bibleitit/sortpriorityservice/SortPriorityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibleitit.sortpriorityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortPriorityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
